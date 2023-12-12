162e7945620dedfad4dec68d0251ccd0bbccfd10
7odadocoding <mahmoud.alnakeeb.js@gmail.com>
2023-12-12 15:41:33 +0200

    fix: resolve ReferenceError in pagination
    - Fixed the issue where 'perPage' was not defined, causing a ReferenceError
    - Implemented the necessary changes to ensure the correct definition and usage of 'perPage'

--- data/seeding.js
+++ data/seeding.js
@@ -1,13 +1,53 @@
 const connectDB = require('../configs/db');
-const Clinic = require('../models/Clinic');
-const fs = require('fs');
+const Article = require('../models/Article');
 
-const dummyClinics = JSON.parse(fs.readFileSync('./data/clinics.json', 'utf-8'));
+const medicalArticles = [
+   {
+      articleTitle: 'Understanding Orthopedic Conditions',
+      abstract: 'A comprehensive guide to common orthopedic conditions and their treatments.',
+      main: {
+         sections: [
+            {
+               sectionTitle: 'Introduction',
+               content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+            },
+            {
+               sectionTitle: 'Common Conditions',
+               content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+            },
+         ],
+      },
+      author: '6575ed7233852f4490703c73', // Replace with the actual ID of the doctor/clinic
+      keywords: ['Orthopedics', 'Bone Health', 'Joint Disorders'],
+      publicationDate: new Date(),
+      views: 150,
+   },
+   {
+      articleTitle: 'Advancements in Neurology Research',
+      abstract: 'Exploring the latest research findings in the field of neurology.',
+      main: {
+         sections: [
+            {
+               sectionTitle: 'Neurological Disorders',
+               content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+            },
+            {
+               sectionTitle: 'Research Findings',
+               content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+            },
+         ],
+      },
+      author: '6575ed7233852f4490703c76', // Replace with the actual ID of the doctor/clinic
+      keywords: ['Neurology', 'Brain Health', 'Neurological Research'],
+      publicationDate: new Date(),
+      views: 120,
+   },
+];
 
 async function seed() {
    connectDB();
-   const newClinics = await Clinic.create(dummyClinics);
-   console.log(newClinics);
+   const newArticles = await Article.create(medicalArticles);
+   console.log(newArticles);
 }
 
 seed().then(() => {
--- services/article.service.js
+++ services/article.service.js
@@ -8,10 +8,14 @@ class ArticleService {
    async getArticles(page = 1, limit = 10) {
       try {
          const skip = (page - 1) * limit;
-         const articles = await this.ArticleModel.find().populate('author', 'doctorName').skip(skip).limit(perPage);
+         const articles = await this.ArticleModel.find().populate({
+            path:'author',
+            select:'doctorName'
+         }).skip(skip).limit(limit);
 
          return articles;
       } catch (error) {
+         console.log('Error:' , error);
          throw error;
       }
    }
