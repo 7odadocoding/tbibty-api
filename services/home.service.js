e5609936308eb9fc3bc974851fc7d5ad6de340d4
7odadocoding <mahmoud.alnakeeb.js@gmail.com>
2023-12-13 16:54:23 +0200

    refactor: HomeService to fetch additional data for top rated doctors, labs, and latest articles

--- services/home.service.js
+++ services/home.service.js
@@ -8,6 +8,7 @@ class HomeService {
    async getTopRatedDoctors() {
       try {
          const doctors = await this.ClinicModel.find({ category: 'CLINIC' })
+            .select(['doctorName', 'specialization', 'degree', 'phone', 'address', 'locationUrl', 'workTimes', 'price'])
             .sort({ reviewsCount: -1, averageRatingValue: -1 })
             .limit(3)
             .exec();
@@ -21,6 +22,7 @@ class HomeService {
    async getTopRatedLabs() {
       try {
          const labs = await this.ClinicModel.find({ category: 'LAB' })
+            .select(['doctorName', 'specialization', 'degree', 'phone', 'address', 'locationUrl', 'workTimes', 'price'])
             .sort({ reviewsCount: -1, averageRatingValue: -1 })
             .limit(3)
             .exec();
@@ -33,7 +35,15 @@ class HomeService {
 
    async getLatestArticles() {
       try {
-         const latestArticles = await Article.find().sort({ publicationDate: -1 }).limit(1).exec();
+         const latestArticles = await Article.find()
+            .select('articleTitle author')
+            .populate({
+               path: 'author',
+               select: 'doctorName',
+            })
+            .sort({ publicationDate: -1 })
+            .limit(1)
+            .exec();
          return latestArticles;
       } catch (error) {
          throw new Error('Failed to get the latest articles');
